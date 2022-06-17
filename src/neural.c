8683892a672d14491d4ef4e29a02e052a461fa2e
Ben Tang <78770681+woderggininder@users.noreply.github.com>
2022-06-17 18:01:39 -0400

    Update neural.c

--- src/neural.c
+++ src/neural.c
@@ -59,27 +59,29 @@ void deleteNeuralNetwork(NeuralNetwork* NN)
 
 void feedForward(NeuralNetwork* NN, double* inputs)
 {
+    // Copying array into input matrix
     for (int i = 0; i < NN->numInput; i++)
     {
         NN->inputs->data[i][0] = inputs[i];
     }
 
+    // Move forward through layers
     for (int i = 0; i < NN->numHiddenLayer; i++)
     {
         deleteMatrix(NN->layers[i]->activations);
         deleteMatrix(NN->layers[i]->z);
         Matrix* zPreBias = matrixMul(NN->layers[i]->w, i == 0 ? NN->inputs : NN->layers[i-1]->activations);
         NN->layers[i]->z = matrixAdd(zPreBias, NN->layers[i]->bias);
-        NN->layers[i]->activations = copyMatrix(NN->layers[i]->z);
 
-        // Activation func for last layer is reserved for softMax
+        // Activation func for last layer is reserved for softMax, others use Leaky ReLU
         if (i != NN->numHiddenLayer-1)
         {
+            NN->layers[i]->activations = copyMatrix(NN->layers[i]->z); // activation takes in z (weight input)
             matrixApply(NN->layers[i]->activations, &lrelu);
         }
         else
         {
-            NN->layers[NN->numHiddenLayer-1]->activations = softMax(NN->layers[NN->numHiddenLayer-1]->activations);
+            NN->layers[i]->activations = softMax(NN->layers[i]->z);
         }
 
         deleteMatrix(zPreBias);
@@ -88,27 +90,29 @@ void feedForward(NeuralNetwork* NN, double* inputs)
 
 void predict(NeuralNetwork* NN, double* inputs)
 {
+    // Copying array into input matrix
     for (int i = 0; i < NN->numInput; i++)
     {
         NN->inputs->data[i][0] = inputs[i];
     }
 
+    // Move forward through layers
     for (int i = 0; i < NN->numHiddenLayer; i++)
     {
         deleteMatrix(NN->layers[i]->activations);
         deleteMatrix(NN->layers[i]->z);
         Matrix* zPreBias = matrixMul(NN->layers[i]->w, i == 0 ? NN->inputs : NN->layers[i-1]->activations);
         NN->layers[i]->z = matrixAdd(zPreBias, NN->layers[i]->bias);
-        NN->layers[i]->activations = copyMatrix(NN->layers[i]->z);
 
-        // Activation func for last layer is reserved for argMax
+        // Activation func for last layer is reserved for argMax. others use Leaky ReLU
         if (i != NN->numHiddenLayer-1)
         {
+            NN->layers[i]->activations = copyMatrix(NN->layers[i]->z);
             matrixApply(NN->layers[i]->activations, &lrelu);
         }
         else
         {
-            NN->layers[NN->numHiddenLayer-1]->activations = argMax(NN->layers[NN->numHiddenLayer-1]->activations);
+            NN->layers[i]->activations = argMax(NN->layers[i]->z);
         }
 
         deleteMatrix(zPreBias);
@@ -117,10 +121,10 @@ void predict(NeuralNetwork* NN, double* inputs)
 
 void backPropogate(NeuralNetwork* NN, double* inputs, double* observedValues)
 {
-    // Updtate the outputs in NN
+    // Updtate the output in NN
     feedForward(NN, inputs);
 
-    // Copy array into matrix
+    // Copying array into matrix
     Matrix* observed = makeMatrix(NN->layers[NN->numHiddenLayer-1]->numNeuron, 1);
     for (int r = 0; r < NN->layers[NN->numHiddenLayer-1]->numNeuron; r++)
     {
@@ -128,32 +132,36 @@ void backPropogate(NeuralNetwork* NN, double* inputs, double* observedValues)
     }
 
     // Compute error of the output layer
+    // Math detail: https://towardsdatascience.com/derivative-of-the-softmax-function-and-the-categorical-cross-entropy-loss-ffceefc081d1
     Matrix** layerErrors = malloc(sizeof(Matrix*) * NN->numHiddenLayer);
-    layerErrors[NN->numHiddenLayer-1] = matrixSub(NN->layers[NN->numHiddenLayer-1]->activations, observed);
-
+    layerErrors[NN->numHiddenLayer-1] = matrixSub(NN->layers[NN->numHiddenLayer-1]->activations, observed); // Derivative of CE w.r.t the z of output layer
     deleteMatrix(observed);
 
-    // Back propogate
+    // Back propogation starting with the second last hidden layer and working backward
+    // Every layer looks at its next layer's errors and calculates its own accordingly
     for (int l = NN->numHiddenLayer-2; l >= 0; l--)
     {
-        Matrix* previousT = matrixTranspose(NN->layers[l+1]->w);
-        Matrix* previousError = matrixMul(previousT, layerErrors[l+1]);
+        Matrix* nextT = matrixTranspose(NN->layers[l+1]->w);
+        Matrix* nextError = matrixMul(nextT, layerErrors[l+1]);
         Matrix* dLrelu = copyMatrix(NN->layers[l]->z);
         matrixApply(dLrelu, &dlrelu);
-        layerErrors[l] = matrixHadaMul(previousError, dLrelu);
+        layerErrors[l] = matrixHadaMul(nextError, dLrelu);
 
-        deleteMatrix(previousT);
-        deleteMatrix(previousError);
+        deleteMatrix(nextT);
+        deleteMatrix(nextError);
         deleteMatrix(dLrelu);
     }
 
     // Calculate gradients for weights and biases
     for (int l = 0; l < NN->numHiddenLayer; l++)
     {
+        // Calculate matrix delta whose dimensions match current layer's w
+        // It represents all gradients of the current layer's w
+        // Math detail: http://neuralnetworksanddeeplearning.com/chap2.html
         Matrix* prevActT = matrixTranspose(l == 0 ? NN->inputs : NN->layers[l-1]->activations);
         Matrix* deltas = matrixMul(layerErrors[l], prevActT);
         
-        // In-place update to current layer's weights
+        // Update to current layer's weights
         for (int r = 0; r < deltas->row; r++)
         {
             for (int c = 0; c < deltas->col; c++)
@@ -162,7 +170,9 @@ void backPropogate(NeuralNetwork* NN, double* inputs, double* observedValues)
             }
         }
 
-        // In-place update to current layer's bias
+        // Update to current layer's biases
+        // Delta for biases happens to be the current error
+        // Math detail: http://neuralnetworksanddeeplearning.com/chap2.html
         for (int i = 0; i < NN->layers[l]->numNeuron; i++)
         {
             NN->layers[l]->bias->data[i][0] -= (0.01 * (layerErrors[l]->data[i][0]));
